75d18c575d92675421ef9f246b6518c5d84676e3
zoovasoup <zulfa.plank@gmail.com>
2023-10-20 11:37:30 +0700

    add javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
-console.log(23+10)
+/*
+const MAX = 57 ;
+let actual = MAX - 13;
+let percentage = actual/MAX;
+
+
+if (age >= 14 && age <= 90) {
+
+}
+
+if (age <= 14 && age >=90){
+
+}
+
+if ( !(age>=14 && age<=90)) {
+
+}
+
+if (age <14 || age > 90) {
+
+}
+*/
+
+let username = prompt('who\'s there?','');
+
+if (username === 'Admin'){
+
+    let pass = prompt('password','');
+
+    if (pass === 'TheMaster') {
+        alert('Welcome!');
+    } else if (pass === 'cancel' || pass === 'null') {
+        alert('cancel');
+    } else { 
+        alert('Wrong password');
+    }
+
+} else if (username === '' || username === null){ 
+    alert('canceled');
+} else {
+    alert('i don\'t know you');
+}
